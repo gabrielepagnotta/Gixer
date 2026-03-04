28b4241f1727d03b754fa0d2114f93988302c1f3
gabrielepagnotta <gabriele.pagnotta1998@gmail.com>
2026-03-04 13:13:41 +0100

    creata struttura html della pagina lista ore

--- Gixer/Gixer_api/models.py
+++ Gixer/Gixer_api/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class OreInserite(models.Model):
     ore_straordinario = models.IntegerField(blank=True, null=True)
     ore_permesso = models.IntegerField(blank=True, null=True)
-    ore_ferie = models.IntegerField(db_column='Ore_ferie', blank=True, null=True)  # Field name made lowercase.
+    ore_ferie = models.IntegerField(db_column='Ore_ferie', blank=True, null=True)
+    data_inserimento_ore = models.DateField(blank=True, null=True)
 
     class Meta:
         managed = False
